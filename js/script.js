9bcb75ee1baa150d4478a75dfe090ee23e8497ad
broke1 <fredswork@list.ru>
2020-10-13 12:27:45 +0300

    menu almost done, rest change size

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 'use strict'
 
 window.addEventListener('load', function() {
-  console.log('sfsdf')
+
+
+  // МЕНЮ
+      
+  Vue.component('menu-items', {
+    props: ['menu'],
+    template: `
+      <a :href="menu.url" :class="menu.class">{{menu.name}}<span></span></a>
+    `
+  })
+
+
+  new Vue({
+    el: ".menu",
+    data: {
+      menu: [
+        {url:"main",name:"Ссылка 1",class:""},
+        {url:"main",name:"Ссылка 2",class:""},
+        {url:"main",name:"Ссылка 3",class:""},
+        {url:"main",name:"Ссылка 4",class:""},
+        {url:"main",name:"Ссылка 5",class:""},
+        {url:"main",name:"Ссылка 6",class:"callback-menu"},
+      ]
+    }
+
+  })
+
+
+
+
+
 
 })
