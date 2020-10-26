ffb9f0e1139199be0a6c0adea99f23d5167d2a46
broke1 <fredswork@list.ru>
2020-10-26 12:35:07 +0300

    try to set touche event

--- js/script.js
+++ js/script.js
@@ -455,23 +455,15 @@ document.addEventListener("touchmove", function (e) {
    }
    if (node.classList.contains('text-block')) {
       if ((e.touches[0].pageX < touchcoord)) {
-        // let items = document.querySelectorAll('.service-item')
-        // let last = items.length - 1
-        // let clone = items[last].cloneNode(true)
-        // clone.style.marginLeft = `-${step}px`
-        // let service_block = items[0].parentElement
-        // service_block.removeChild(items[last])
-        // service_block.insertBefore(clone, items[0])
-        // setTimeout( () => {
-        //   clone.style.marginLeft = `${margin}px`
-        // },50)
         let items = document.querySelectorAll('.service-item')
-        let clone = items[0].cloneNode(true)
+        let last = items.length - 1
+        let clone = items[last].cloneNode(true)
+        clone.style.marginLeft = `-${step}px`
         let service_block = items[0].parentElement
-        items[0].style.marginLeft = `-${step}px`
-        setTimeout(() => {
-          service_block.removeChild(items[0])
-          service_block.appendChild(clone)
+        service_block.removeChild(items[last])
+        service_block.insertBefore(clone, items[0])
+        setTimeout( () => {
+          clone.style.marginLeft = `${margin}px`
         },300)
       } else {
         let items = document.querySelectorAll('.service-item')
