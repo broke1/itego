9111792882fc2bff41dda1e2f7edc068f0a59cd0
broke1 <fredswork@list.ru>
2020-10-26 12:15:42 +0300

    try to set touche event

--- js/script.js
+++ js/script.js
@@ -456,7 +456,7 @@ document.addEventListener("touchmove", function (e) {
    if (node.classList.contains('text-block')) {
      
       if ((e.touches[0].pageX < touchcoord)) {
-        
+        alert('left')
         let items = document.querySelectorAll('.service-item')
         let last = items.length - 1
         let clone = items[last].cloneNode(true)
@@ -468,6 +468,7 @@ document.addEventListener("touchmove", function (e) {
           clone.style.marginLeft = `${margin}px`
         },50)
       } else {
+        alert('right')
         let items = document.querySelectorAll('.service-item')
         let clone = items[0].cloneNode(true)
         let service_block = items[0].parentElement
