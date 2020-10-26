f9178a09ef9d8040b089928f706c1f3b9aa26e1d
broke1 <fredswork@list.ru>
2020-10-26 12:32:56 +0300

    try to set touche event

--- js/script.js
+++ js/script.js
@@ -455,16 +455,24 @@ document.addEventListener("touchmove", function (e) {
    }
    if (node.classList.contains('text-block')) {
       if ((e.touches[0].pageX < touchcoord)) {
+        // let items = document.querySelectorAll('.service-item')
+        // let last = items.length - 1
+        // let clone = items[last].cloneNode(true)
+        // clone.style.marginLeft = `-${step}px`
+        // let service_block = items[0].parentElement
+        // service_block.removeChild(items[last])
+        // service_block.insertBefore(clone, items[0])
+        // setTimeout( () => {
+        //   clone.style.marginLeft = `${margin}px`
+        // },50)
         let items = document.querySelectorAll('.service-item')
-        let last = items.length - 1
-        let clone = items[last].cloneNode(true)
-        clone.style.marginLeft = `-${step}px`
+        let clone = items[0].cloneNode(true)
         let service_block = items[0].parentElement
-        service_block.removeChild(items[last])
-        service_block.insertBefore(clone, items[0])
-        setTimeout( () => {
-          clone.style.marginLeft = `${margin}px`
-        },50)
+        items[0].style.marginLeft = `-${step}px`
+        setTimeout(() => {
+          service_block.removeChild(items[0])
+          service_block.appendChild(clone)
+        },300)
       } else {
         let items = document.querySelectorAll('.service-item')
         let clone = items[0].cloneNode(true)
