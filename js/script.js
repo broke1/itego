4ab253842da0317d34f2e16acf737f4344d29385
broke1 <fredswork@list.ru>
2020-10-27 08:34:16 +0300

    another try to touch

--- js/script.js
+++ js/script.js
@@ -484,26 +484,28 @@ document.addEventListener("touchend", function (e) {
       node = node.parentElement
     }
     if (node.classList.contains('text-block')) {
-      if ((e.touches[0].pageX < touchcoord)) {
-        let items = document.querySelectorAll('.service-item')
-        let last = items.length - 1
-        let clone = items[last].cloneNode(true)
-        clone.style.marginLeft = `-${step}px`
-        let service_block = items[0].parentElement
-        service_block.removeChild(items[last])
-        service_block.insertBefore(clone, items[0])
-        setTimeout( () => {
-          clone.style.marginLeft = `${margin}px`
-        },50)
+      if ((e.changedTouches[0].pageX < touchcoord)) {
+        alert('left')
+        // let items = document.querySelectorAll('.service-item')
+        // let last = items.length - 1
+        // let clone = items[last].cloneNode(true)
+        // clone.style.marginLeft = `-${step}px`
+        // let service_block = items[0].parentElement
+        // service_block.removeChild(items[last])
+        // service_block.insertBefore(clone, items[0])
+        // setTimeout( () => {
+        //   clone.style.marginLeft = `${margin}px`
+        // },50)
       } else {
-        let items = document.querySelectorAll('.service-item')
-        let clone = items[0].cloneNode(true)
-        let service_block = items[0].parentElement
-        items[0].style.marginLeft = `-${step}px`
-        setTimeout(() => {
-          service_block.removeChild(items[0])
-          service_block.appendChild(clone)
-        },300)
+        alert('right')
+        // let items = document.querySelectorAll('.service-item')
+        // let clone = items[0].cloneNode(true)
+        // let service_block = items[0].parentElement
+        // items[0].style.marginLeft = `-${step}px`
+        // setTimeout(() => {
+        //   service_block.removeChild(items[0])
+        //   service_block.appendChild(clone)
+        // },300)
       }
     } 
 })
